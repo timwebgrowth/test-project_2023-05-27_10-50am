037a4f6a548ab10a37a331eaca770cb52f6c4dbb
tim <tim@adzoola.com>
2024-05-27 22:41:32 +0300

    Last push

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+// 'https://exchangeratesapi.io/Has been needing a key to access
+// Normally I am would not put a key in gitHub, but considering that this is a test project, here it is
 // 1273ecc6304d648f9d4995fed388c88f, 250 requests only
 // If expire, use your own or register new key with 'https://exchangeratesapi.io/'
 return [
